bacf9f9dc5c3ee3a379066adf9d2a2a0ba2ade79
강지민 <84773954+jmk307@users.noreply.github.com>
2024-03-11 15:09:38 +0900

    feat : 초기설정

---
+++ src/main/java/com/osanvalley/moamail/MoamailApplication.java
@@ -0,0 +1,13 @@
+package com.osanvalley.moamail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoamailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoamailApplication.class, args);
+	}
+
+}
