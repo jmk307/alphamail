9710a90f974db17339c73b8f8b0f997f35560346
강지민 <84773954+jmk307@users.noreply.github.com>
2024-03-11 15:09:38 +0900

    feat : 초기설정

---
+++ src/test/java/com/osanvalley/moamail/MoamailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osanvalley.moamail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoamailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
