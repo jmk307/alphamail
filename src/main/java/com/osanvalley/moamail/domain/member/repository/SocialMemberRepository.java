7dcb97e21661e2d0533972e02944526246659f99
jmk307 <jimin112688@gmail.com>
2024-06-01 19:21:20 +0900

    feat : 소셜 계정 연동

--- src/main/java/com/osanvalley/moamail/domain/member/repository/SocialMemberRepository.java
+++ src/main/java/com/osanvalley/moamail/domain/member/repository/SocialMemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.osanvalley.moamail.domain.member.entity.Member;
+import com.osanvalley.moamail.domain.member.model.RegisterType;
 import com.osanvalley.moamail.domain.member.model.Social;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,7 +12,7 @@ import com.osanvalley.moamail.domain.member.entity.SocialMember;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SocialMemberRepository extends JpaRepository<SocialMember, Long> {
-    Optional<SocialMember> findBySocialId(String socialId);
+    Optional<SocialMember> findBySocialIdAndMember_RegisterType(String socialId, RegisterType registerType);
 
     Optional<SocialMember> findByMember_AuthIdAndSocial(String authId, Social social);
 }
